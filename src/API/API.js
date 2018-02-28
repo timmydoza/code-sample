420aba7f5aecacc4c189fb47ea2a68b382abca6c
Tim Mendoza <timmydoza@gmail.com>
2018-02-28 15:16:03 -0500

    hook up to real data.  lots of cars!

---
+++ src/API/API.js
@@ -0,0 +1,9 @@
+const applyKeys = data => data.map( (item, key) => {
+  return {key, ...item}
+});
+
+export default () => {
+  return fetch('https://gist.githubusercontent.com/creatifyme/2a334c00a117097bfdb47f031edf292c/raw/efb52ecf1cf92e2261f504ec7639c68b5ff390bd/cars.json')
+    .then( data => data.json() )
+    .then( data => applyKeys(data))
+}
--- src/containers/CarFinder/CarFinder.jsx
+++ src/containers/CarFinder/CarFinder.jsx
@@ -2,32 +2,12 @@ import React, { Component } from 'react';
 import CarList from '../../components/CarList/CarList';
 import CarModal from '../../components/CarModal/CarModal';
 import styles from './CarFinder.css';
+import API from '../../API/API';
 
 class CarFinder extends Component {
 
   state = {
-    cars: [{
-      "key": 1,
-      "year": 2013,
-      "make": "Kia",
-      "model": "Optima",
-      "mileage": 24235,
-      "drivetrain": "FWD",
-      "bodytype": "sedan",
-      "image_url": "http://www.optimaforums.com/forum/attachments/new-member-introductions/11137d1347548855-new-2013-kia-optima-sx-l-titanium-photo.jpg",
-      "created_at": "2016-10-14T20:13:22.586Z"
-      },
-      {
-      "key": 2,
-      "year": 2013,
-      "make": "Hyundai",
-      "model": "Accent",
-      "mileage": 21587,
-      "drivetrain": "FWD",
-      "bodytype": "sedan",
-      "image_url": "http://www.conceptcarz.com/images/Hyundai/2013-Hyundai-Accent-Sedan-Image-01.jpg",
-      "created_at": "2016-11-14T20:13:22.586Z"
-      }],
+    cars: [],
     filters: {
       year: null,
       mileage: null,
@@ -43,10 +23,16 @@ class CarFinder extends Component {
     });
   }
 
-
+  componentDidMount = () => {
+    API().then(cars => {
+      this.setState({
+        cars
+      })
+    });
+  }
 
   render() {
-    
+
     return (
       <main className={styles.grid}>
         <CarList 
