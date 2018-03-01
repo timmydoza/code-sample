49bba9f7eb1a221e29e2c1526e17f5dde1a23403
Tim Mendoza <timmydoza@gmail.com>
2018-03-01 02:54:29 -0500

    add test for PaginationButtons

---
+++ src/components/CarList/PaginationButtons/PaginationButtons.test.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import PaginationButtons from './PaginationButtons';
+
+describe('the PaginationButtons component', () => {
+  let paginationButtons, setPage;
+
+  beforeEach(() => {
+    setPage = jest.fn();
+    paginationButtons = renderer
+      .create(<PaginationButtons currentPage="4" totalpages="11" setPage={setPage}/>)
+      .toJSON()
+    debugger;
+  });
+
+  it('renders correctly', () => {
+    expect(paginationButtons).toMatchSnapshot();
+  });
+
+  it('has working buttons', () => {
+    paginationButtons.children[0].props.onClick();
+    paginationButtons.children[2].props.onClick(); //Click the buttons
+    expect(setPage.mock.calls).toEqual([[-1],[1]]);
+  })
+
+});
