caa1237841a2f2fae0ff5662dec4030bd82da49a
Tim Mendoza <timmydoza@gmail.com>
2018-03-01 14:33:49 -0500

    update API mock and CarFinder tests

--- src/containers/CarFinder/CarFinder.test.jsx
+++ src/containers/CarFinder/CarFinder.test.jsx
@@ -9,11 +9,34 @@ describe('the CarFinder component', () => {
 
   beforeEach(() => {
     carFinder = renderer.create(<CarFinder />);
-    debugger;
   });
 
   it('renders correctly', () => {
     expect(carFinder.toJSON()).toMatchSnapshot();
+  });
+
+  it('should reset the page if a filter occurs', () => {
+    const instance = carFinder.getInstance();
+    instance.setPage(1);
+    instance.setSearch('testing');
+    expect(instance.state.currentPage).toBe(1);
+  });
+
+  it('should reset the page if a sort occurs', () => {
+    const instance = carFinder.getInstance();
+    instance.setPage(1);
+    instance.setSort('mileage');
+    expect(instance.state.currentPage).toBe(1);
+  });
+
+  it('should not let pagination go out of range', () => {
+    const instance = carFinder.getInstance();
+    instance.setPage(-1);
+    expect(instance.state.currentPage).toBe(1);
+    instance.setPage(1);
+    instance.setPage(1);
+    instance.setPage(1);
+    expect(instance.state.currentPage).toBe(2);
   });
 
 });
