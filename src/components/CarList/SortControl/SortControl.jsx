72d29392517c29171a23cdccb268818a97375224
Tim Mendoza <timmydoza@gmail.com>
2018-02-28 23:03:19 -0500

    get rid of old stuff and fix linter messages;

--- src/components/CarList/SortControl/SortControl.jsx
+++ src/components/CarList/SortControl/SortControl.jsx
@@ -1,17 +1,6 @@
 import React from 'react';
-import styles from './SortControl.css'
-import classnames from 'classnames/bind';
-
-let cx = classnames.bind(styles);
 
 const SortControl = (props) => {
-  const classes = cx(
-    'SortControl',
-    {
-      'ascending': props.mode === 'ascending',
-      'descending': props.mode === 'descending'
-    }
-  )
 
   return (
     <div>
