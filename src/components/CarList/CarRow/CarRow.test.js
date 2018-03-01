9982a79b3de2b8770067b183afde03a5ffa24894
Tim Mendoza <timmydoza@gmail.com>
2018-03-01 01:50:37 -0500

    add react-test-renderer.  add snapshot test for CarRow

---
+++ src/components/CarList/CarRow/CarRow.test.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import CarRow from './CarRow';
+
+const testCar = {
+  make: 'TestMake',
+  model: 'TestModel',
+  year: 1999,
+  mileage: 1234
+}
+
+it('renders correctly', () => {
+  const tree = renderer
+    .create(<CarRow {...testCar} clickHandler={Function.prototype}/>)
+    .toJSON();
+  expect(tree).toMatchSnapshot();
+});
