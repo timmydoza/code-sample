a133a9fd95f4397ae60fb689535b1ce61a1241f1
Tim Mendoza <timmydoza@gmail.com>
2018-03-01 20:06:06 -0500

    more linting

--- src/components/CarList/SearchField/SearchField.jsx
+++ src/components/CarList/SearchField/SearchField.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-const SearchField = (props) => {
-  return (
+const SearchField = props =>
+  (
     <div>
       <span>Search: </span>
-      <input type="text" onChange={e => props.setSearch(e.target.value) } /> 
+      <input type="text" onChange={e => props.setSearch(e.target.value)} />
     </div>
   );
-}
+
+SearchField.propTypes = {
+  setSearch: PropTypes.func.isRequired,
+};
 
 export default SearchField;
--- src/components/CarList/SortControl/SortControl.jsx
+++ src/components/CarList/SortControl/SortControl.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import styles from './SortControl.css';
 
-const SortControl = (props) => {
-
-  return (
+const SortControl = props =>
+  (
     <div className={styles.sortControl} >
       <label>
-        Sort By: 
-        <select onChange={(e) => props.setSort(e.target.value)}>
+        Sort By:
+        <select onChange={e => props.setSort(e.target.value)}>
           <option value="year">Year - Oldest to Newest</option>
           <option value="mileage">Mileage - Highest to Lowest</option>
           <option value="created_at">Date Listed - Newest to Oldest</option>
@@ -16,6 +16,9 @@ const SortControl = (props) => {
 
     </div>
   );
-}
+
+SortControl.propTypes = {
+  setSort: PropTypes.func.isRequired,
+};
 
 export default SortControl;
