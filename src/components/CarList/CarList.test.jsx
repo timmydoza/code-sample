17f838f7c3b24fab0c60d268956681060e571eca
Tim Mendoza <timmydoza@gmail.com>
2018-03-01 19:59:05 -0500

    clean up carlist test

--- src/components/CarList/CarList.test.jsx
+++ src/components/CarList/CarList.test.jsx
@@ -14,38 +14,28 @@ const testProps = {
       make: 'Testmake',
       model: 'Testmodel',
       year: 1999,
-      mileage: 20000
+      mileage: 20000,
     },
     {
       key: 2,
       make: 'Testmake2',
       model: 'Testmodel2',
       year: 1998,
-      mileage: 20001
-    }
-  ]
+      mileage: 20001,
+    },
+  ],
 };
 
 describe('the CarList component', () => {
-  // let paginationButtons, setPage;
-
-  // beforeEach(() => {
-  //   setPage = jest.fn();
-  //   paginationButtons = renderer
-  //     .create(<PaginationButtons currentPage="4" totalPages="11" setPage={setPage}/>)
-  //     .toJSON()
-  // });
-
   it('renders correctly', () => {
     const carList = renderer.create(<CarList {...testProps} />).toJSON();
     expect(carList).toMatchSnapshot();
   });
 
   it('should show a message if there are no results to show', () => {
-    const testData = {...testProps};
+    const testData = { ...testProps };
     testData.cars = [];
     const carList = renderer.create(<CarList {...testData} />).toJSON();
     expect(carList).toMatchSnapshot();
   });
-
 });
