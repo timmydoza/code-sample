9ee2ea2585650f029b8e8048825478d607a8836c
Tim Mendoza <timmydoza@gmail.com>
2018-03-07 16:58:20 -0500

    add test to utils

--- src/utils/utils.test.js
+++ src/utils/utils.test.js
@@ -226,4 +226,37 @@ describe('the sortAndFilter function', () => {
 
     expect(sortAndFilter(testCars, 'mileage', 'j')).toEqual(verification);
   });
+
+  it('should not mutate the car array', () => {
+    const testCars = [
+      {
+        year: 2014,
+        make: 'Jaguar',
+        model: 'XK',
+        mileage: 9852,
+        drivetrain: 'RWD',
+        bodytype: 'convertible',
+        image_url: 'http://st.motortrend.com/uploads/sites/10/2015/09/2014-Jaguar-XKR-S-GT-front-three-quarter-in-motion-02.jpg',
+        created_at: '2016-10-14T20:13:22.586Z',
+      }, {
+        year: 2013,
+        make: 'Audi',
+        model: 'A5',
+        mileage: 17216,
+        image_url: 'http://st.motortrend.com/uploads/sites/5/2012/07/2013-Audi-A5-front-three-quarter-in-motion.jpg',
+        created_at: '2016-10-14T20:13:22.586Z',
+      }, {
+        year: 2015,
+        make: 'Jeep',
+        model: 'Wrangler Unlimited',
+        mileage: 19000,
+        image_url: 'http://blog.caranddriver.com/wp-content/uploads/2014/07/2013-Jeep-Wrangler-Unlimited-Rubicon-10th-Anniversary-Edition-PLACEMENT.jpg',
+        created_at: '2016-10-14T20:13:22.586Z',
+      },
+    ];
+
+    const testCarsCopy = testCars.slice();
+    const sorted = sortAndFilter(testCars, 'year', 'J');
+    expect(testCars).toEqual(testCarsCopy);
+  });
 });
