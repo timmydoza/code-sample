54f09902f8d08c2cf0b72f7735ecb83b5ca843cf
Tim Mendoza <timmydoza@gmail.com>
2018-03-07 23:52:29 -0500

    add test for app.jsx

---
+++ src/App.test.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import App from './App';
+
+jest.mock('./API/API');
+
+describe('the carFiner app', () => {
+  it('should render correctly', (done) => {
+    const app = renderer.create(<App/>)
+    setImmediate(() => { // To deal with the promise in mock API;
+      expect(app).toMatchSnapshot();
+      done();
+    });
+  });
+});
