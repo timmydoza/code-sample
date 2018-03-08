398f5bfbb3a4befe3c8c1ce26d9a4493fc7754b9
Tim Mendoza <timmydoza@gmail.com>
2018-03-07 23:52:16 -0500

    rework app.jsx and index.js ;

--- src/index.js
+++ src/index.js
@@ -1,15 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, compose, applyMiddleware } from 'redux';
-import thunk from 'redux-thunk';
 import './index.css';
-import CarFinder from './containers/CarFinder/CarFinder';
+import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import rootReducer from './store/reducers';
 
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-const store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunk)));
-
-ReactDOM.render(<Provider store={store}><CarFinder /></Provider>, document.getElementById('root'));
+ReactDOM.render(<App/>, document.getElementById('root'));
 registerServiceWorker();
